a12deaae1a10c3c617c9ddc365dbd0256128985d
michaelryan0509 <37228274+michaelryan0509@users.noreply.github.com>
2018-10-15 10:52:05 -0700

    Code review feedback

--- UninstallMcAfee.ps1
+++ UninstallMcAfee.ps1
@@ -1,17 +1,20 @@
 $ErrorActionPreference = "Stop"
 
+# Must first uninstall frminst.exe in order to unlock the full uninstall for McAfee
+
+# Find frminst.exe if it exists
 $frminstFindings = @(Get-Childitem -Path C:\ -Recurse -Filter 'frminst.exe' -File -Force -ErrorAction SilentlyContinue)
 $frminst = $frminstFindings[0].FullName
 Write-Host $frminst
 if($frminst -eq $null)
 {
-    Write-Host 'Could not find frminst.exe' 
+    Write-Host 'Could not find frminst.exe'
     Exit
 }
-else
-{
-    Start-Process $frminst -Wait -NoNewWindow -ArgumentList @("/forceuninstall")
-}
 
+# Run frminst.exe /forceuninstall
+Start-Process $frminst -Wait -NoNewWindow -ArgumentList @("/forceuninstall")
+
+# Find and fully uninstall McAfee application
 $application = Get-WmiObject Win32_Product -filter "Name='McAfee VirusScan Enterprise'"
 $application.Uninstall()
